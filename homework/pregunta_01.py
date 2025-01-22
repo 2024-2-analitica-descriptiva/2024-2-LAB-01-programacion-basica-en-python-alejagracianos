788514f8c0369a1d936a6951b7535d0229589ee0
alejagracianos <55900435+alejagracianos@users.noreply.github.com>
2025-01-21 20:30:56 -0500

    LAB1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,13 +4,18 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
 
     Rta/
     214
-
     """
+    total = 0
+    with open("files/input/data.csv", "r", encoding="utf-8") as file:
+        for line in file:
+            columns = line.strip().split("\t")
+            total += int(columns[1])
+    return total
+
+print(pregunta_01())
